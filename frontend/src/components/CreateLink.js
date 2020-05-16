beaa648115e8b2b8e5952bfb10641df4349793c3
quentinchampenois <quentin.champenois7@gmail.com>
2020-05-16 16:22:03 +0200

    add clipboard copy library

--- frontend/src/components/CreateLink.js
+++ frontend/src/components/CreateLink.js
@@ -3,14 +3,16 @@ import React from 'react';
 class LinkForm extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {value: ''};
+        this.state = {};
 
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
     }
 
     handleChange(event) {
-        this.setState({value: event.target.value});
+        this.setState({
+            [event.target.name]: event.target.value
+        });
     }
 
     handleSubmit(event) {
@@ -28,17 +30,17 @@ class LinkForm extends React.Component {
                 <form onSubmit={this.handleSubmit}>
                     <label>
                         Name:
-                        <input type="text" value={this.state.value} onChange={this.handleChange} />
+                        <input type="text" name="name" value={this.state.value} onChange={this.handleChange} />
                     </label>
                     <label>
                         magnet link:
-                        <input type="text" value={this.state.value} onChange={this.handleChange} />
+                        <input type="text" name={"link"} value={this.state.value} onChange={this.handleChange} />
                     </label>
                     <label>
                         description:
-                        <input type="text" value={this.state.value} onChange={this.handleChange} />
+                        <textarea name={"description"} value={this.state.value} onChange={this.handleChange} />
                     </label>
-                    <input type="submit" value="Submit" />
+                    <button type={"submit"} className="hollow button" href="#">Save !</button>
                 </form>
                 </div>
             </div>
--- frontend/src/components/MagnetLink.js
+++ frontend/src/components/MagnetLink.js
@@ -1,19 +1,31 @@
 import React from 'react';
+import {CopyToClipboard} from 'react-copy-to-clipboard';
 
 class MagnetLink extends React.Component {
 
+    constructor(props) {
+        super(props)
+        this.state = {
+            value: this.props.magnetLink,
+            copied: false,
+        };
+    }
+
     render() {
-        const magnetDescription = this.props.description;
         return (
             <div className="card">
                 <div className="card-divider">
                     <h3>{this.props.name}</h3>
                 </div>
                 <div className="card-section">
-                    {magnetDescription != null && magnetDescription.length > 0 &&
-                        <p>{magnetDescription}</p>
+                    {this.props.description != null && this.props.description.length > 0 &&
+                        <p>{this.props.description}</p>
                     }
-                    <b>{this.props.magnetLink}</b>
+                    <CopyToClipboard text={this.state.value}
+                                     onCopy={() => this.setState({copied: true})}>
+                        <span>{this.props.magnetLink}</span>
+                    </CopyToClipboard>
+                    {this.state.copied ? <span style={{color: 'red'}}>Copied.</span> : null}
                 </div>
             </div>
         );
